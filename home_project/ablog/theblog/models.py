68654151fd8f39ee8f9c13c98c615d9e31ac7924
Elizabeth <elizabeth.saharova@mail.ru>
2021-01-09 19:18:32 +0300

    post and category

--- home_project/ablog/theblog/models.py
+++ home_project/ablog/theblog/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
+
+class Category(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __str__(self):
+    return str(self.name)
+  
+  def get_absolute_url(self):
+    return reverse('article-detail', args=(str(self.pk)))
+    #return reverse('home')
 
 class Post(models.Model):
   title = models.CharField(max_length=255)
   author = models.ForeignKey(User, on_delete=models.CASCADE)
   body = models.TextField()
   date = models.DateTimeField(auto_now_add=True)
+  category = models.CharField(max_length=255, default='lifestyle')
 
   def __str__(self):
     return str(self.title) + ' | ' + str(self.author)
--- home_project/ablog/theblog/urls.py
+++ home_project/ablog/theblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create-post', AddPostView.as_view(), name='create-post'),
     path('article/<int:pk>/edit', UpdatePostView.as_view(), name='update-post'),
     path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete-post'),
+    path('category/<str:cats>', CategoryView, name='category'),
+    path('add-category', AddCategoryView.as_view(), name='add-category'),
 ]
